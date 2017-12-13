5c6c35e04522521d79624e719647e0b585ca3c63
Krzych <krzych.pierzchala@gmail.com>
2017-12-13 10:16:24 +0100

    Transform to ES6 syntax

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
-var express = require("express");
-var app = express();
+const express = require('express');
+const app = express();
 
 app.use(express.static("assets"));
 
-app.get("/", function(req, res) {
-  res.sendFile("/index.html");
-});
+app.get("/", (req, res) => res.sendFile("/index.html"));
 
-app.get("/userform", function(req, res) {
-  var response = {
+app.get("/userform", (req, res) => {
+  const response = {
     first_name: req.query.first_name,
     last_name: req.query.last_name
   };
   res.end(JSON.stringify(response));
 });
 
-var server = app.listen(3000, "localhost", function() {
-  var host = server.address().address;
-  var port = server.address().port;
+const server = app.listen(3000, "localhost", () => {
+  const host = server.address().address;
+  const port = server.address().port;
   console.log(
     "Przykładowa aplikacja nasłuchuje na http://" + host + ":" + port
   );
